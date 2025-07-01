3e130ebecc5b3ca812392d641c070f6d786384a7
nurfariezza <nurfariezza@yahoo.com.my>
2025-07-01 12:57:15 +0800

    Apply .gitignore to untrack ignored files

---
+++ settings.py
@@ -0,0 +1,8 @@
+port = 587  # For SSL
+smtp_server = "mail.redtone.com"
+sender_email = "frim@biz.redtone.com"  # Enter your address
+password = '6elk@hkU'
+theme = "FRIM Notification"
+sender = "noreply_frim@redtone.com"
+
+#con = mysql.connector.connect(host='10.80.10.39', user='frim', password='P@ssw0rd2288',  database='FRIM')
